f2d5c838418e408af4d5ae19da3fd0d7c0ac26a6
Michael Pearce <mdpearce@gmail.com>
2016-04-21 16:26:36 +1000

    Initial layout of converter

---
+++ app/src/main/java/com/neaniesoft/currentlyconvert/conversion/ConversionContract.java
@@ -0,0 +1,21 @@
+package com.neaniesoft.currentlyconvert.conversion;
+
+import com.neaniesoft.currentlyconvert.model.Currency;
+
+/**
+ * Created by mdpearce on 21/04/16.
+ */
+public interface ConversionContract {
+    interface View {
+        void showResult(String result);
+        void setFromCurrencyDisplay(Currency from);
+        void setToCurrencyDisplay(Currency to);
+    }
+
+    interface UserActionsListener {
+        void performConversion(String amountString);
+        void swapCurrencies(Currency from, Currency to);
+        void setFromCurrency(Currency from);
+        void setToCurrency(Currency to);
+    }
+}
