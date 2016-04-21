c79fc982707607d65e9669bc845ee2b642af5380
Michael Pearce <mdpearce@gmail.com>
2016-04-21 17:15:11 +1000

    Get views

--- app/src/main/java/com/neaniesoft/concurrency/conversion/ConversionFragment.java
+++ app/src/main/java/com/neaniesoft/concurrency/conversion/ConversionFragment.java
@@ -2,21 +2,41 @@ package com.neaniesoft.concurrency.conversion;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.neaniesoft.concurrency.R;
 import com.neaniesoft.concurrency.model.Currency;
 
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ConversionFragment extends Fragment implements ConversionContract.View {
 
+    @Bind(R.id.spinner_from_currency)
+    Spinner mSpinnerFrom;
+
+    @Bind(R.id.spinner_to_currency)
+    Spinner mSpinnerTo;
+
+    @Bind(R.id.edit_from)
+    EditText mEditTextFrom;
+
+    @Bind(R.id.text_result)
+    TextView mTextResult;
+
+    private ConversionContract.UserActionsListener mActionsListener;
 
     public ConversionFragment() {
         // Required empty public constructor
@@ -26,8 +46,19 @@ public class ConversionFragment extends Fragment implements ConversionContract.V
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_conversion, container, false);
+        View view = inflater.inflate(R.layout.fragment_conversion, container, false);
+
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setRetainInstance(true);
+
+        mActionsListener = new ConversionPresenter(this);
     }
 
     @Override
--- app/src/main/java/com/neaniesoft/concurrency/conversion/ConversionPresenter.java
+++ app/src/main/java/com/neaniesoft/concurrency/conversion/ConversionPresenter.java
@@ -1,11 +1,20 @@
 package com.neaniesoft.concurrency.conversion;
 
+import android.support.annotation.NonNull;
+
 import com.neaniesoft.concurrency.model.Currency;
 
 /**
  * Created by mdpearce on 21/04/16.
  */
 public class ConversionPresenter implements ConversionContract.UserActionsListener {
+
+    private final ConversionContract.View mConversionView;
+
+    public ConversionPresenter(@NonNull ConversionContract.View conversionView) {
+        mConversionView = conversionView;
+    }
+
     @Override
     public void performConversion(String amountString) {
 
