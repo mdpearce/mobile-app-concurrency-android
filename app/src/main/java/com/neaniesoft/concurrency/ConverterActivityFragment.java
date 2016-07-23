96118724a4ff83fc04f4d798f74de22053800def
Michael Pearce <mdpearce@gmail.com>
2016-07-23 17:03:51 +1000

    Add empty studio project

---
+++ app/src/main/java/com/neaniesoft/concurrency/ConverterActivityFragment.java
@@ -0,0 +1,22 @@
+package com.neaniesoft.concurrency;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ConverterActivityFragment extends Fragment {
+
+    public ConverterActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_converter, container, false);
+    }
+}
