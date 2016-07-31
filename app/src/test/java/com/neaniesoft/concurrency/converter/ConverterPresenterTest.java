efd0e61704504ea199e15be751922b14e88e2035
Michael Pearce <mdpearce@gmail.com>
2016-07-31 11:14:53 +1000

    Add test for loading currencies from repository

--- app/src/test/java/com/neaniesoft/concurrency/converter/ConverterPresenterTest.java
+++ app/src/test/java/com/neaniesoft/concurrency/converter/ConverterPresenterTest.java
@@ -1,16 +1,26 @@
 package com.neaniesoft.concurrency.converter;
 
+import com.neaniesoft.concurrency.data.CurrenciesDataSource;
 import com.neaniesoft.concurrency.data.CurrenciesRepository;
+import com.neaniesoft.concurrency.data.Currency;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 /**
@@ -20,6 +30,8 @@ import static org.junit.Assert.assertEquals;
 public class ConverterPresenterTest {
 
     private static Map<String, String> CURRENCIES_MAP;
+    private static List<Currency> CURRENCIES;
+    private static Date DATE;
 
     private static double DELTA = 0.001;
 
@@ -29,11 +41,16 @@ public class ConverterPresenterTest {
     @Mock
     ConverterContract.View mConverterView;
 
+    @Captor
+    private ArgumentCaptor<CurrenciesDataSource.LoadCurrenciesCallback> mLoadCurrenciesCallbackCaptor;
+
     private ConverterPresenter mConverterPresenter;
 
     @Before
     public void setupConverterPresenter() {
         MockitoAnnotations.initMocks(this);
+
+        DATE = new Date();
 
         CURRENCIES_MAP = new HashMap<>();
         CURRENCIES_MAP.put("USD", "US Dollar");
@@ -41,7 +58,27 @@ public class ConverterPresenterTest {
         CURRENCIES_MAP.put("GBP", "Pound Sterling");
         CURRENCIES_MAP.put("EUR", "Euro");
 
+        CURRENCIES = new ArrayList<>();
+        CURRENCIES.add(new Currency("USD", 1.0));
+        CURRENCIES.add(new Currency("AUD", 1.3302));
+        CURRENCIES.add(new Currency("GBP", 0.75947));
+        CURRENCIES.add(new Currency("EUR", 0.89985));
+
+        // Always return true for isActive
+        when(mConverterView.isActive()).thenReturn(true);
+
         mConverterPresenter = new ConverterPresenter(mConverterView, mCurrenciesRepository, CURRENCIES_MAP);
+    }
+
+    @Test
+    public void loadCurrenciesFromRepositoryAndLoadIntoView() {
+        mConverterPresenter.loadCurrencies();
+        verify(mCurrenciesRepository).getCurrencies(mLoadCurrenciesCallbackCaptor.capture());
+        mLoadCurrenciesCallbackCaptor.getValue().onCurrenciesLoaded(CURRENCIES, DATE);
+
+        ArgumentCaptor<List> showCurrenciesArgumentCaptor = ArgumentCaptor.forClass(List.class);
+        verify(mConverterView).setAvailableCurrencies(showCurrenciesArgumentCaptor.capture(), CURRENCIES_MAP);
+        assertTrue(showCurrenciesArgumentCaptor.getValue().size() == 4);
     }
 
     @Test
